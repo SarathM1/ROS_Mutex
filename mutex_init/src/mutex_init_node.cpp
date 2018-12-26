23a09ebd6a1157c194954354c040682fb33ce1f1
Home PC <sarathm00@gmail.com>
2018-12-26 19:48:08 +0530

    Final commit; Added 3rd node

--- mutex_init/src/mutex_init_node.cpp
+++ mutex_init/src/mutex_init_node.cpp
@@ -6,12 +6,15 @@ int main(int argc, char **argv) {
     // This type of initialisation requires C++11
     named_mutex named_mtx1{open_or_create, "mtx1"};
     named_mutex named_mtx2{open_or_create, "mtx2"};
+    named_mutex named_mtx3{open_or_create, "mtx3"};
 
     // Start locking each mutex in exact order
     ROS_INFO("Locking mtx1");
     named_mtx1.lock();
     ROS_INFO("Locking mtx2");
     named_mtx2.lock();
+    ROS_INFO("Locking mtx3");
+    named_mtx3.lock();
 
     // Unlock only the first mutex so that node1 starts to run
     ROS_INFO("\tUnlocking mtx1");
--- node2_mutex/src/node2_mutex_node.cpp
+++ node2_mutex/src/node2_mutex_node.cpp
@@ -6,6 +6,7 @@ using namespace boost::interprocess;
 int main(int argc, char **argv) {
     // This type of initialisation requires C++11
     named_mutex named_mtx2{open_or_create, "mtx2"};
+    named_mutex named_mtx3{open_or_create, "mtx3"};
     ROS_INFO("Node2 Waiting for lock to release");
     named_mtx2.lock();
     ROS_INFO("Node2 running. .");
@@ -19,5 +20,6 @@ int main(int argc, char **argv) {
     }
     named_mtx2.unlock();
     named_mutex::remove("mtx2");
+    named_mtx3.unlock();
     return 0;
 }
