ac2c025e0fe8dc799253b29730898c929b412233
Home PC <sarathm00@gmail.com>
2018-12-26 19:35:28 +0530

    Renamed nodes

--- other_nodes/src/node3_mutex_node.cpp
+++ other_nodes/src/node3_mutex_node.cpp
@@ -5,19 +5,19 @@ using namespace boost::interprocess;
 
 int main(int argc, char **argv) {
     // This type of initialisation requires C++11
-    named_mutex named_mtx2{open_or_create, "mtx2"};
-    ROS_INFO("Node2 Waiting for lock to release");
-    named_mtx2.lock();
-    ROS_INFO("Node2 running. .");
+    named_mutex named_mtx3{open_or_create, "mtx3"};
+    ROS_INFO("Node3 Waiting for lock to release");
+    named_mtx3.lock();
+    ROS_INFO("Node3 running. .");
     ros::init(argc, argv, "talker");
     ros::NodeHandle nh;
     ros::Rate loop_rate(1);
     for(int i=5;i>0;i--)
     {
-        ROS_INFO("Node2 %d", i);
+        ROS_INFO("Node3 %d", i);
         loop_rate.sleep();
     }
-    named_mtx2.unlock();
-    named_mutex::remove("mtx2");
+    named_mtx3.unlock();
+    named_mutex::remove("mtx3");
     return 0;
 }
