6ff66b0fb2dee51e1cfd387dee49ed4564b8c93d
Home PC <sarathm00@gmail.com>
2018-12-26 17:17:20 +0530

    First commit; Working

---
+++ node1_mutex/src/node1_mutex_node.cpp
@@ -0,0 +1,33 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include "boost/interprocess/sync/named_mutex.hpp"
+#include "boost/interprocess/sync/scoped_lock.hpp"
+#include "boost/thread/thread.hpp"
+
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+#include <sstream>
+using namespace boost::interprocess;
+
+int main(int argc, char **argv) {
+    // This type of initialisation requires C++11
+    named_mutex named_mtx{open_or_create, "mtx"};
+    ROS_INFO("Node1 Waiting for lock to release");
+    named_mtx.lock();
+    ROS_INFO("Node1 running. .");
+    ros::init(argc, argv, "talker");
+    ros::NodeHandle n;
+    ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+
+    ros::Rate loop_rate(1);
+    for(int i=5;i>0;i--)
+    {
+        ROS_INFO("%d", i);
+        loop_rate.sleep();
+    }
+    named_mtx.unlock();
+    return 0;
+}
