94a88a4bed037aed6f058838b852751d4e7c6469
Home PC <sarathm00@gmail.com>
2018-12-26 19:30:05 +0530

    First commit

---
+++ other_nodes/src/node2_mutex_node.cpp
@@ -0,0 +1,23 @@
+#include "boost/interprocess/sync/named_mutex.hpp"
+#include "ros/ros.h"
+
+using namespace boost::interprocess;
+
+int main(int argc, char **argv) {
+    // This type of initialisation requires C++11
+    named_mutex named_mtx2{open_or_create, "mtx2"};
+    ROS_INFO("Node2 Waiting for lock to release");
+    named_mtx2.lock();
+    ROS_INFO("Node2 running. .");
+    ros::init(argc, argv, "talker");
+    ros::NodeHandle nh;
+    ros::Rate loop_rate(1);
+    for(int i=5;i>0;i--)
+    {
+        ROS_INFO("Node2 %d", i);
+        loop_rate.sleep();
+    }
+    named_mtx2.unlock();
+    named_mutex::remove("mtx2");
+    return 0;
+}
